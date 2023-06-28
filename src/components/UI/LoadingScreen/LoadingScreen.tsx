4d47560f297b92b86ef734088d68cd49f6cae9a8
Tvoncher <tvincher@mail.ru>
2023-06-28 12:29:25 +0100

    ADD:new loadingScreen

--- src/components/UI/LoadingScreen/LoadingScreen.tsx
+++ src/components/UI/LoadingScreen/LoadingScreen.tsx
@@ -1,15 +1,14 @@
 import { FC } from "react";
 import "./LoadingScreen.css";
 
-/* looks exactly as greeting screen and seems like you can delete it
-but you really need this structure for future improvements like
-creating really goodlooking loading screen and changing greeting to real authorisation page */
-
 const LoadingScreen: FC = () => {
   return (
-    <div className="loader">
-      <img className="loader__img" src="images/logo.png" alt="logo" />
-      <button className="loader__button">Play</button>
+    <div className="loadingScreen">
+      <img
+        className="loadingScreen__img"
+        src="images/loading.jpg"
+        alt="loadingScreen"
+      />
     </div>
   );
 };
